dc994f44b53acbad227354c06ee17a545297e9a4
Badia Sellam <sellamb2009@hotmail.fr>
2023-10-29 10:58:50 -0400

    add portfolio projet

---
+++ forms/contact.php
@@ -0,0 +1,31 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+ 
+  $receiving_email_address = 'sellamb2009@hotmail.fr';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new $php_email_form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
